625936f3b8b3394ecdedbce41c5cef1285124bdc
adrianna-spyt <71889255+adrianna-spyt@users.noreply.github.com>
2022-12-01 02:45:45 +0100

    porządki

--- litery_v2.py
+++ litery_v2.py
@@ -14,10 +14,7 @@ pg.display.flip()
 ekran.fill((0,0,0))
 font = pg.font.SysFont("Arial",150)
 ileliter=0
-zacznij_od=0
 czas_poczatkowy=time.time()
-licznik=pg.time.get_ticks()
-clock = pg.time.Clock()
 timer_event = pg.USEREVENT+1
 pg.time.set_timer(timer_event, 1000)
 stan="litery"
@@ -33,10 +30,10 @@ while running:
             if ileliter>=10:
                 stan="przerwa"
             if stan=="litery":
-                font = pg.font.SysFont("Arial",150)
                 poczatek_wyswietlania=time.time()
                 with open("litery_czas.txt", "a") as myfile:
                     myfile.write(lista_liter[indeks] + ', ' + str(poczatek_wyswietlania) + '\n')
+                font = pg.font.SysFont("Arial",150)
                 aktualna_litera = font.render(lista_liter[indeks],True, (255,255,255))
                 ekran.fill((0,0,0))
                 ekran.blit(aktualna_litera,(410,220))
@@ -48,9 +45,9 @@ while running:
                     indeks+=1
             if stan=="przerwa":
                 poczatek_mrugania=time.time()
-                font = pg.font.SysFont("Arial",70)
                 with open("litery_czas.txt", "a") as myfile:
                     myfile.write('Czas na mruganie, ' + str(poczatek_mrugania) + '\n')
+                font = pg.font.SysFont("Arial",70)
                 ekran.fill((0,0,0))
                 tekst=font.render("TERAZ MRUGAJ DOWOLNIE",True, (255,255,255))
                 ekran.blit(tekst,(60,150))
